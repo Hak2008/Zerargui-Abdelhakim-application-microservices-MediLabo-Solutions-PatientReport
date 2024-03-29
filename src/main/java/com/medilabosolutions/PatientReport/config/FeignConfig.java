7c11043405da1f1a4cb6c0ef3728271815eca713
Zerargui <hakim.zerargui@hotmail.com>
2024-03-29 15:29:29 +0100

    Mise en place de SpringSecurity, FeignConfig

---
+++ src/main/java/com/medilabosolutions/PatientReport/config/FeignConfig.java
@@ -0,0 +1,15 @@
+package com.medilabosolutions.PatientReport.config;
+
+import feign.RequestInterceptor;
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public RequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("user", "user");
+    }
+}
--- src/main/java/com/medilabosolutions/PatientReport/proxies/PatientInfoServiceProxy.java
+++ src/main/java/com/medilabosolutions/PatientReport/proxies/PatientInfoServiceProxy.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "PatientInfoService", url = "localhost:8080")
+
+@FeignClient(name = "PatientInfoService", url = "Localhost:8080")
 public interface PatientInfoServiceProxy {
 
     @GetMapping("/patient/details/{id}")
